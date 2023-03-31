1661c4d505834a7a68377050664f37dc41bc7ab2
milo cohen-elyanow <milocohenelyanow@gmail.com>
2023-03-30 23:57:51 -0500

    important notes for Oauth cross platform tokens

--- server/test.js
+++ server/test.js
@@ -2,3 +2,29 @@
 
 //this script will test multiple assets from the api to make sure they all work and will function on its own.
 
+//base url:  https://osu.ppy.sh/api/[version]/  
+// in this case we will use v2 since v1 is depreciated 
+// -https://osu.ppy.sh/api/v2/
+
+// Client ID: 21334
+// Client Secret: Rr0KuWEfhrtL1svoMfLMFcr29fzNL5Iu9asJooap
+
+// calls will be made with axios
+//
+
+
+// https://osu.ppy.sh/oauth/authorize
+//this get req will provide user with their auth token for our app to run with their auth token! callback should maybe work automaticly ?
+
+//EXAMPLE REQ 
+
+/*Example request:
+curl --request GET \
+    --get "https://osu.ppy.sh/oauth/authorize?client_id=1&redirect_uri=http%3A%2F%2Flocalhost%3A4000&res
+
+
+
+
+    */
+
+const axios = require("axios").default;
